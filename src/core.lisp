02d7539c502ed63560513985b6cb4622bc5ca7a0
callsamu <samuelbree@gmail.com>
2021-08-29 15:42:43 +0000

    Large refactoring; opcode disassembling was implemented; introduction of the rutils package; changed package name.

--- src/core.lisp
+++ src/core.lisp
@@ -13,19 +13,17 @@
 (deftype int-8 ()  '(unsigned-byte 8))
 (deftype int-16 ()  '(unsigned-byte 16))
 
-;;; Data Structures
+;;; Main data structure
 (defstruct chip
   (Vr (make-array 16 :element-type 'int-8)
    :type (simple-array int-8 (16)))
   (Ir 0 :type int-16)
   (delayr 0 :type int-8)
   (soundr 0 :type int-8)
-  (counter 0 :type int-16)
+  (counter 200 :type int-16)
   (stack (make-array 16 :element-type 'int-16
 			:fill-pointer 0))
   (memory (make-array +mem-size+ :element-type 'int-8)))
-
-(defstruct instruction asm func)
 
 ;;; Globals
 (defparameter *chip*
@@ -34,8 +32,7 @@
 (defparameter *cpu-function-table*
   (make-hash-table :size 37))
 
-;;; General Helper functions
-(declaim (inline hexadecimal-string-p))
+;; General Helper functions
 (defun hexadecimal-string-p (hs)
   (and (stringp hs)
        (every (lambda (c) (digit-char-p c 16)) hs)))
@@ -51,32 +48,36 @@
 (defun get-bit (bit-pos num)
   (ldb (byte 1 bit-pos) num))
 
+(declaim (inline concat-bytes))
+(defun concat-bytes (b1 b2)
+  (+ (ash b1 8) b2))
+
 (declaim (inline charlist->string))
 (defun charlist->string (charlist)
   (coerce charlist 'string))
 
-(declaim (inline 8_+))
-(defun 8_+ (a b)
+(declaim (inline byte-add))
+(defun byte-add (a b)
   "Addition between eight bit integers with
    with the possibility of overflow"
   (let ((sum (+ a b)))
     (values (mod sum 256)
 	    (if (> sum 255) 1 0))))
 
-(declaim (inline 8_-))
-(defun 8_- (a b)
+(declaim (inline byte-sub))
+(defun byte-sub (a b)
   "Subtraction between eight bit integers with
    with the possibility of overflow"
   (values (mod (- a b) 256)
 	  (if (> b a) 1 0)))
 
-(defun 8_<< (a)
+(defun byte-lshift (a)
   "Left bitwise shift which also
    returns the most significant bit."
   (values (ash a 1)
 	  (get-bit 0 a)))
 	    
-(defun 8_>> (a)
+(defun byte-rshift (a)
   "Right bitwise shift which also
    returns the least significant bit."
   (values (ash a -1)
@@ -109,10 +110,128 @@
 	do (setf (aref dest j)
 		 (aref src i))))
 
+;; Assembly Related
+
+(defclass instruction ()
+  ((assembly
+    :initarg :asm
+    :initform
+    (error
+     ":asm parameter must be supplied to
+      instance of class instruction")
+    :type list
+    :reader assembly
+    :documentation
+     "Sexp assembly representation
+      of the instruction.")
+   (function
+    :initarg :func
+    :initform 
+    (error
+     ":func parameter must be supplied to
+      instance of class instruction")
+    :type function
+    :reader func
+    :documentation
+    "Function object which
+     actually implements
+     the instruction")))
+
+(defmethod initialize-instance :after ((this instruction) &key)
+  (with-slots ((asm assembly) (func function)) this
+    (setf asm (sanitize-sexp-asm asm))))
+
+(defgeneric disassembly (instruction &optional variables)
+  (:documentation
+    "Disassembly method for any object which can
+     refer to an instruction value"))
+
+(defmethod disassembly ((opcode integer) &optional v)
+  (declare (ignorable v))
+  (destructuring-bind (instruction vars)
+      (retrieve-instruction opcode)
+    (disassembly instruction vars)))
+
+(defmethod disassembly ((this instruction) &optional vars)
+  (format-sexp-asm (assembly this) vars))
+
+(defun sanitize-sexp-asm (asm)
+  (labels ((first-symbol-char (s)
+	     (char (symbol-name s) 0))
+           (variablep (s)
+	     (let ((fcs (first-symbol-char s)))
+	       (or (eql fcs #\@)
+		   (eql fcs #\$)))))
+    (assert (and (not (variablep (first asm)))
+                 (every #'symbolp asm)))
+    (map-if #'variablep #'first-symbol-char asm))))
+
+(defun format-sexp-asm (asm vars)
+  (flet ((format-var (prefix)
+	   (let ((directive (if (char= prefix #\@)
+				"~c~1,'0x"
+				"~c~4,'0x")))
+	     (intern (format nil directive prefix (pop vars)) :keyword))))
+  (map-if #'characterp #'format-var asm)))
+
+;; Instruction Table
+(defconstant +mask0+ #xFFFF)
+(defconstant +mask1+ #xF0FF)
+(defconstant +mask2+ #xF00F)
+(defconstant +mask3+ #xF000)
+
+(defmacro with-byte-chunks (large-byte chunks &body body)
+   "Binds a variable number of symbols to a specified
+    to a form which retrieves a specified chunk of the
+    parameter <large-byte>." 
+  `(let ,(loop for (name size off) in chunks
+	   collect `(,name (ldb (byte ,size ,off)
+	     		        ,large-byte)))
+      ,@body))
+ 
+(declaim (inline classify-opcode))
+(defun classify-opcode (op)
+  "Returns the opcode variables and a
+   constant value for masking them."
+  (with-byte-chunks op
+      ((id 4 12)
+       (x 4 8)
+       (y 4 4)
+       (n 4 0)
+       (kk 8 0)
+       (nnn 12 0))
+    (ecase id
+      (#x0               (list +mask0+ nil))
+      (#xD               (list +mask3+ x y n))
+      ((#xE #xF)         (list +mask1+ x))
+      ((#x5 #x8 #x9)     (list +mask2+ x y))
+      ((#x1 #x2 #xA #xB) (list +mask3+ nnn))
+      ((#x3 #x4 #x6 #x7 #xC) (list +mask3+ x kk)))))
+
+(let ((table (make-hash-table :test 'eq :size 47)))
+  (defun add-instruction (opcode instruction)
+    "Adds an <instruction> with key <opcode> to the table"
+    (setf (gethash opcode table) instruction))
+
+  (defun retrieve-instruction (opcode)
+    "Looks for the opcode ANDed with one the masks
+     returned by the classification function.
+     If the lookup result is nil, an error is signaled,
+     otherwise, it is added to a pair with the instruction 
+     variables and returned it."
+    (destructuring-bind (mask &rest vars)
+        (classify-opcode opcode)
+      (if-it (gethash (logand mask opcode) table)
+	(list it vars) 
+	(error "Instruction not found: opcode ~4,'0x. is invalid" opcode)))))
+
+
+
 ;;; Instruction DSL
+
 ;; Those macros/functions are responsible for implementing a small DSL
 ;; for defining the instruction set. 
-(defun reduce-op-tokens (op-tokens)
+(defun reduce-opcode-tokens (op-tokens)
   "Receives a list of strings as input, substitutes each
    character of strings which don't contain hexnumbers for
    zeroes, concatenates all strings into a single one and
@@ -127,17 +246,16 @@
 	(apply #'concatenate 'string %)
 	(parse-integer % :radix 16)))
 	
-
 (defun parse-opcode-string (string)
   "Parses an opcode string, which contains 
    symbols and hexnumbers, returning a list
    that holds its numeric representation, 
-   produced by #reduce-op-tokens# and a list
+   produced by #reduce-opcode-tokens# and a list
    of the symbols.
    Ex: \"5 x kk\" => (#x5000 (X KK))"
   (let ((splitted (split #\SPACE string)))
-    (list (reduce-op-tokens splitted)
-          (mapcar #'mksym (remove-if #'hexadecimal-string-p splitted))))))
+    (list (reduce-opcode-tokens splitted)
+          (mapcar #'mksym (remove-if #'hexadecimal-string-p splitted)))))
 
 (defun |#[]-opcode-string-reader| (stream char1 char2)
  "A read macro for representing opcodes containing variables.
@@ -152,10 +270,10 @@
   it so in read-time seemed easier and more reasonable."
   (declare (ignorable char1 char2))
     (parse-opcode-string
-         (charlist->string
-          (iter (:for c := (read-char stream nil))
-          (:until (eql c #\]))
-          (:collect c)))))
+      (charlist->string
+       (iter (:for c := (read-char stream nil))
+         (:until (eql c #\]))
+         (:collect c)))))
 
 (set-dispatch-macro-character #\# #\[
   (function |#[]-opcode-string-reader|))
@@ -173,7 +291,7 @@
 		  collect (setfable f))
      ,@body)))
 
-  
+ 
 (defmacro with-chip-registers (chip-obj &body body)
   "Wraps the body in both a macrolet and symbol-macrolet
    which define bindings for accessing the chip object's main slots.
@@ -195,57 +313,50 @@
 		       
 
 (defmacro definstruction (name opcode &body body)
- "Definstruction creates an anonymous function whose
-  parameter list is composed of the symbols contained in <opcode>
-  and wraps its body in a with-chip-registers macro. The
-  new function is then stored in the #func# slot of the 
-  #instruction# struct along with <name> on the asm slot.
-    This new instruction is then stored in *cpu-function-table*
-  with a key defined by the result of a reduce on <opcode>
-  where each number or symbol is treated as digits of a
-  single hexadecimal value, and each character of a symbol
-  count as a zero. For instance, (1 2 nnn) would become #x12000.
-    The <name> parameter must be a sexp representation of the
-  instruction's assembly code. See #make-asm-sexp# for more details."
-  `(flet ((self ,(second opcode)
-            (with-chip-registers *chip* ,@body)))
-     (setf (gethash ,(first opcode) *cpu-function-table*)
-	   (make-instruction :asm ',(sanitize-sexp-asm name) :func #'self)))))
+  (destructuring-bind (key vars) opcode
+    (with-gensyms (chip)
+      `(add-instruction
+	 ,key
+         (make-instance 'instruction
+           :asm ',name
+           :func (lambda (,chip ,@vars)
+	 	  (with-chip-registers ,chip ,@body)))))))
 
-;; Functions for disassembling
-
-(defun sanitize-sexp-asm (asm)
-  (labels ((first-symbol-char (s)
-	     (char (symbol-name s) 0))
-           (variablep (s)
-	     (let ((fcs (first-symbol-char s)))
-	       (or (eql fcs #\@) (eql fcs #\$)))))
-    (unless (and (every #'symbolp asm)
-	         (not (variablep (first asm))))
-      (error "Invalid sexp-asm."))
-    (map-if #'variablep #'first-symbol-char asm)))
-
-(defun format-sexp-asm (asm vars)
-  (map-if #'characterp
-	  (lambda (x)
-	    (intern (format nil "~c~4,'0x" x (pop vars)) :keyword))
-	  asm))
-
+;; Core functions
 (defun load-rom (chip pathname)
+  (with-open-file (fd pathname :element-type 'unsigned-byte)
+    (read-sequence (chip-memory chip) fd :start 200))))
+		     
+(defun fetch (chip)
   (with-chip-registers chip
-    (with-open-file (fd pathname :element-type 'unsigned-byte)
-      (loop for a = (read-byte fd nil)
-	    for i from 200
-	    while a do (setf (@MEM i) a)))))
-  
+    (let ((b1 (@MEM @PC))
+	  (b2 (@MEM (1+ @PC))))
+      (concat-bytes b1 b2))))
+	     
+(defun execute (chip opcode)
+  (destructuring-bind (ins vars) (retrieve-instruction opcode)
+    (print (format-sexp-asm (instruction-asm ins) vars))))
+    (apply (instruction-func ins) chip vars)
+ 
+(defun disasm (opcode)
+  (multiple-value-bind (ins vars) (retrieve-instruction opcode)
+    (format-sexp-asm (instruction-asm ins) vars)))
+
+(defun cycle (chip)
+  (execute (fetch chip))
+  (print (chip-counter chip))
+  (incf (chip-counter chip) 2))
+
 (defun run (chip)
-  (with-chip-registers chip
-    (setf @PC 200)
-    (loop ())))
+  (let ((*chip* chip))
+    (loop (cycle chip) (sleep 1))))
 		       
 ;; CHIP-8 instructions
 ;; See http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
 ;; For a full documentation of those.
+
+(definstruction (:SYS) #[0 nnn]
+  (print "Unsuported instruction: SYS"))
 
 (definstruction (:CLS) #[00E0]
   (print "CLS not implemented."))
@@ -253,7 +364,7 @@
 (definstruction (:RET) #[00EE]
   (setf @PC (vector-pop @STACK)))
 
-(definstruction (:LD :$addr) #[1 nnn]
+(definstruction (:JMP :$addr) #[1 nnn]
   (setf @PC nnn))
 
 (definstruction (:CALL :$addr) #[2 nnn]
@@ -275,16 +386,16 @@
 (definstruction (:SNE :@x :@y) #[9 x y 0]
   (branch-if (/= (@V x) (@V y))))
 
-(definstruction SNE-y #[E x 9E]
+(definstruction (:SKP :@x) #[E x 9E]
   ;;(key-pressed-p (@V x)))
   (print "key-pressed-p not implemented."))
 
-(definstruction SNE-y #[#xE x #x9E]
+(definstruction (:SKNP :@x) #[E x A1]
   ;;(not (key-pressed-p (@V x))))
-  (print "key-pressed-p not implemented.")))
+  (print "key-pressed-p not implemented."))
 
-(definstruction (:LD :@x :$byte) #[6 x byte]
-  (setf (@V x) byte))
+(definstruction (:LD :@x :$byte) #[6 x kk]
+  (setf (@V x) kk))
 
 (definstruction (:ADD :@x :$byte) #[7 x kk]
   (incf (@V x) kk))
@@ -301,26 +412,32 @@
 (definstruction (:XOR :@x :@y) #[8 x y 3]
   (setf (@V x) (logxor (@V x) (@V y))))
 
-(macrolet ((defarithmetical (name args body)
-	     `(definstruction ,name ,args
-	        (multiple-value-bind (value flag) ,body
-		  (setf (@V ,(cadr body)) value)
-		  (setf @VF flag)))))
+(defmacro do-arithmetic (operation x &rest ys)
+  "Due to free variable injections, should 
+   only be used inside with-chip forms.
+   Apply the operation, set the first
+   xth register to the result and the flag
+   register to the flag value."
+  (with-gensyms (result flag)
+    `(multiple-value-bind (,result ,flag)
+	                  (apply #',operation ,x ,ys)
+       (setf (@V ,x) ,result)
+       (setf (@V #xF) ,flag))))
 
-  (defarithmetical ADD-y #[8 x y 4]
-    (8_+ (x y))
+(definstruction (:ADD :@x :@y) #[8 x y 4]
+  (do-arithmetic byte-add x y))
 
-  (defarithmetical SUB-y #[8 x y 5]
-    (8_- x y))
+(definstruction (:SUB :@x :@y) #[8 x y 5]
+  (do-arithmetic byte-sub x y))
 
-  (defarithmetical SHR-y #[8 x y 6]
-    (8_>> x y))
+(definstruction (:SHR :@x :@y) #[8 x y 6]
+  (do-arithmetic byte-rshift x y))
 
-  (defarithmetical SUBNy #[8 x y 7]
-    ((lambda (x y) (8_- y x)) x y))
+(definstruction (:SUBN :@x :@y) #[8 x y 7]
+  (do-arithmetic byte-sub x y))
 
-  (defarithmetical SHL-y #[8 x y #xE]
-    (8_<< x y))))
+(definstruction (:SHL :@x :@y) #[8 x y E]
+  (do-arithmetic byte-lshift x y))
 
 (definstruction (:LD :I :$byte) #[A nnn]
   (setf @I nnn))
@@ -337,79 +454,31 @@
 (definstruction (:LD :@x :DT) #[F x 07]
   (setf (@V x) @DT))
 
-(definstruction RD-key #[F x 0A]
+(definstruction (:LD :@x :K) #[F x 0A]
   ;; (setf (@V x) (read-key)))
   (print "Read-key not implemented."))
 
-(definstruction LD-delay (#xF 4b!x #x15)
+(definstruction (:LD :DT :@x) #[F x 15]
   (setf @DT (@V x)))
 
-(definstruction LD-sound (#xF 4b!x #x18)
+(definstruction (:LD :ST :@x) #[F x 18]
   (setf @ST (@V x)))
 
-(definstruction ADD-I (#xF 4b!x #x1E)
+;; Versão nova
+(definstruction (:ADD :I :@x) #[F x 1E]
   (setf @I (+ (@V x) @I)))
 
-(definstruction RD-font (#xF 4b!x #x29))
+(definstruction (:LD :F :@x) #[F x 29]
   ;;Not yet implemented.
+  (print "Read-font not implemented"))
 
-(definstruction RD-BCD (#xF x #x33)
-  (loop for d in (digits 3 (@V x))
+(definstruction (:LD :B :@x) #[F x 33]
+  (loop for d in (list-n-digits 3 (@V x))
 	for a from @I
-	(setf (@MEM a) d)))
+	do (setf (@MEM a) d)))
 
-(definstruction LD-[i] (#xF x #x55)
+(definstruction (:LD :[I] :@[0..x]) #[F x 55]
   (memory-copy @V @MEM x :dest-offset @I))
 
-(definstruction RD-[i] (#xF x #x65)
+(definstruction (:LD :@[0..x] :[I]) #[F x 65]
   (memory-copy @MEM @V x :src-offset @I))
-
-
-
-
-(defmacro with-byte-chunks (large-byte chunks &body body)
-   "Binds a variable number of symbols to a specified\
-    to a form which retrieves a specified chunk of the\
-    parameter <large-byte>." 
-  `(symbol-macrolet
-       ,(loop for (name size off) in chunks
-	      collect `(,name (ldb (byte ,size ,off)
-				   ,large-byte)))
-     ,@body))
-
-(defconstant +mask0+ #xFFFF)
-(defconstant +mask1+ #xFF0F)
-(defconstant +mask2+ #xF00F)
-(defconstant +mask3+ #xF000)
-
-(declaim (inline classify-opcode))
-(defun classify-opcode (op)
-  "Returns the opcode variables and a\
-   constant value for masking them."
-  (with-byte-chunks op
-      ((id 4 12)
-       (x 4 8)
-       (y 4 4)
-       (n 4 0)
-       (kk 8 0)
-       (nnn 12 0))
-    (case id
-      (#x0               (list +mask0+ nil))
-      (#xC               (list +mask3+ x kk n))
-      ((#xD #xF)         (list +mask1+ x))
-      ((#x5 #x8 #x9)     (list +mask2+ x y))
-      ((#x1 #x2 #xA #xB) (list +mask3+ nnn))
-      ((#x3 #x4 #x6 #x7) (list +mask3+ x kk)))))
-
-(defun disasm (op)
-  (destructuring-bind (mask &rest args) (classify-opcode op)
-    (format-sexp-asm (instruction-asm (gethash (logand mask op) *cpu-function-table*)) args)))
-
-    
-
-;(defun process-opcode (chip op)
-;  "Call the opcode associated function along
-;   with it's arguments."
-;  (destructuring-bind (mask &rest args) (classify-opcode op)
-;    (apply (gethash (logand op mask) *cpu-function-table*) (cons op args))))
-		    
